5095ac8a2e9e620f55f8ce8336da202961efc30c
warrenspringgay-dotcom <warrenspringgay@gmail.com>
2026-05-30 15:37:09 +0100

    Launch Stonehouse's website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const siteUrl = "https://www.stonehousesfishandchips.co.uk";
 
@@ -58,7 +59,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en-GB">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
